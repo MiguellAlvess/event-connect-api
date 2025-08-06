a2e727b559419b1211e25c940dfedeb2adae2c20
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-06 14:34:43 -0300

    test: ensure create event use case throw an error if ownerId is not a uuid

--- src/application/create-event.test.ts
+++ src/application/create-event.test.ts
@@ -1,7 +1,7 @@
 import { EventRepositoryDatabase } from "../infra/repository/event-repository.js"
 import { CreateEvent } from "./create-event.js"
 
-describe("Create Event", () => {
+describe("Create Event Use Case", () => {
   const createEvent = new CreateEvent(new EventRepositoryDatabase())
 
   test("should create a event successfully", async () => {
@@ -18,5 +18,17 @@ describe("Create Event", () => {
     expect(output.name).toBe(input.name)
     expect(output.ticketPriceInCents).toBe(input.ticketPriceInCents)
     expect(output.ownerId).toBe(input.ownerId)
+  })
+  test("Deve lançar um erro se o ownerId não for um UUID", async () => {
+    const input = {
+      name: "Show",
+      ticketPriceInCents: 1000,
+      latitude: -90,
+      longitude: -180,
+      date: new Date(new Date().setHours(new Date().getHours() + 1)),
+      ownerId: "invalid-uuid",
+    }
+    const output = createEvent.execute(input)
+    await expect(output).rejects.toThrow(new Error("Invalid ownerId"))
   })
 })
