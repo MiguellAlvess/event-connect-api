2693906b64afa0e1a77ffc2fe30804ddea7ac0d3
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-06 15:03:59 -0300

    feat: add post /events route

---
+++ src/api.ts
@@ -0,0 +1,33 @@
+import express from "express"
+import { EventRepositoryDatabase } from "./infra/repository/event-repository.js"
+import { CreateEvent } from "./application/create-event.js"
+
+const app = express()
+
+app.use(express.json())
+
+app.post("/events", async (req, res) => {
+  const { name, ticketPriceInCents, latitude, longitude, date, ownerId } =
+    req.body
+  try {
+    const eventRepositoryDatabase = new EventRepositoryDatabase()
+    const createEvent = new CreateEvent(eventRepositoryDatabase)
+    const event = await createEvent.execute({
+      date: new Date(date),
+      name,
+      ticketPriceInCents,
+      latitude,
+      longitude,
+      ownerId,
+    })
+    return res.status(201).json(event)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    console.error(error)
+    return res.status(400).json({ message: error.message })
+  }
+})
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
