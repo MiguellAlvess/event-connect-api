7106acf7442f2cb9ec9e706a7fc8e062a15f654c
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-07 10:02:12 -0300

    feat: add method to fetch event by date, latitude and longitude

--- src/infra/repository/event-repository.ts
+++ src/infra/repository/event-repository.ts
@@ -3,6 +3,7 @@ import { drizzle } from "drizzle-orm/node-postgres"
 import * as schema from "../db/schema.js"
 import { EventRepository } from "../../application/create-event.js"
 import { OnSiteEvent } from "../../domain/on-site-event.js"
+import { and, eq } from "drizzle-orm"
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined")
@@ -11,6 +12,31 @@ if (!process.env.DATABASE_URL) {
 const db = drizzle(process.env.DATABASE_URL, { schema })
 
 export class EventRepositoryDatabase implements EventRepository {
+  async getByDateLatAndLong(params: {
+    date: Date
+    latitude: number
+    longitude: number
+  }): Promise<OnSiteEvent | null> {
+    const output = await db.query.eventsTable.findFirst({
+      where: and(
+        eq(schema.eventsTable.date, params.date),
+        eq(schema.eventsTable.latitude, params.latitude.toString()),
+        eq(schema.eventsTable.longitude, params.longitude.toString())
+      ),
+    })
+    if (!output) {
+      return null
+    }
+    return {
+      id: output.id,
+      ownerId: output.ownerId,
+      name: output.name,
+      ticketPriceInCents: output.ticketPriceInCents,
+      date: output.date,
+      latitude: Number(output.latitude),
+      longitude: Number(output.longitude),
+    }
+  }
   async create(input: OnSiteEvent): Promise<OnSiteEvent> {
     const [output] = await db
       .insert(schema.eventsTable)
