2d03c766bdb42775b8d35dfa4e727beb08660b12
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-05 10:10:58 -0300

    test: update event creation test to use correct server port

---
+++ src/infra/controller/api.test.ts
@@ -0,0 +1,25 @@
+import axios from "axios"
+
+axios.defaults.validateStatus = () => true
+
+describe("POST/events", () => {
+  test("should create a event successfully", async () => {
+    const input = {
+      name: "Party sunset",
+      ticketPriceInCents: 1000,
+      latitude: -90.0,
+      longitude: -180,
+      date: new Date(
+        new Date().setHours(new Date().getHours() + 1)
+      ).toISOString(),
+      ownerId: crypto.randomUUID(),
+    }
+    const response = await axios.post("http://localhost:8080/events", input)
+    expect(response.status).toBe(201)
+    expect(response.data.name).toBe(input.name)
+    expect(response.data.ticketPriceInCents).toBe(input.ticketPriceInCents)
+    // expect(response.data.latitude).toBe(input.latitude)
+    // expect(response.data.longitude).toBe(input.longitude)
+    expect(response.data.ownerId).toBe(input.ownerId)
+  })
+})
