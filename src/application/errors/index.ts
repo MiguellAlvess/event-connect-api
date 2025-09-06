b042cd5b8e90c3bd500de2b582e7ae18a277228a
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-09-06 09:25:08 -0300

    feat: add custom error classes and error codes

--- src/application/errors/index.ts
+++ src/application/errors/index.ts
@@ -1,6 +1,34 @@
-export class InvalidOwnerIdError extends Error {
-  code = "INVALID_OWNER_ID"
+export enum ErrorCode {
+  INVALID_PARAMETER = "INVALID_PARAMETER",
+  INVALID_DATE = "INVALID_DATE",
+  NOT_FOUND = "NOT_FOUND",
+  EVENT_ALREADY_EXISTS = "EVENT_ALREADY_EXISTS",
+}
+
+export class InvalidParameterError extends Error {
+  code = ErrorCode.INVALID_PARAMETER
+  constructor(parameter: string) {
+    super(`Invalid parameter: ${parameter}`)
+  }
+}
+
+export class NotFoundError extends Error {
+  code = ErrorCode.NOT_FOUND
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+export class UnauthorizedError extends Error {
+  code = ErrorCode.NOT_FOUND
+  constructor(message: string) {
+    super(message)
+  }
+}
+
+export class EventAlreadyExistsError extends Error {
+  code = ErrorCode.EVENT_ALREADY_EXISTS
   constructor() {
-    super("Invalid ownerId")
+    super("Event already exists")
   }
 }
